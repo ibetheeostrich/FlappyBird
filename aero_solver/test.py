33bc4acbc85da5a5b87f5909c99e3b4eecaaed49
ibetheeostrich <ibetheeostrich@gmail.com>
2024-08-01 14:12:36 +1000

    solver verified for ideal flat plate

--- aero_solver/test.py
+++ aero_solver/test.py
@@ -1,20 +1,80 @@
-import math
-import scipy
-import scipy.integrate
+import numpy as np
+import scipy.integrate as inte
+import potential_flow as pot
 
-def f():
 
-    return lambda x,y: x**2 + y**1
+# Initialise vortex blob parameters
+N = 1
+Gamma_N = [0]
+eta_N = [0]
+xi_N = [0]
+v_core = 0
 
-def g(x):
-    return x
+# Initialise problem
+U_ref = 10.0
+alpha_eff = np.deg2rad(10)
+c = 1
 
-def fg():
+# Testing Functions
 
-    a = f()
+W_0 = pot.W_0(alpha_eff, U_ref)
 
-    return lambda x: a(x,3)
 
-a = fg()
+# Computing A_0 in fourier series of vorticity distribution on the bound vortex
+A_0 = 0
 
-print(a(1))
+for n in range(N):
+
+    if n == 0:
+
+        integrand_0 = lambda theta, t: W_0(theta,t  ) / U_ref
+
+        A_0_int = inte.quad(integrand_0, 0.0, np.pi,args = (0.0))
+        A_0 = - 1.0 / np.pi * A_0_int[0]
+
+    else:
+        Gamma_n = Gamma_N[n]
+        eta_n   = eta_N[n]
+        xi_n    = xi_N[n]
+        dndeta = pot.dndeta(Gamma_n, eta_n, xi_n, v_core, alpha_eff, c)
+
+        integrand_n = lambda theta: dndeta(theta) / U_ref
+
+        A_0_int = inte.quad(integrand_n, 0.0, np.pi)
+
+        A_0 = A_0 - 1.0 / np.pi * A_0_int[0]
+
+# Computing A_1 in fourier series of vorticity distribution on the bound vortex
+A_1 = 0
+
+for n in range(N):
+
+    if n == 0:
+
+        integrand_0 = lambda theta, t: W_0(theta,t) * np.cos(theta) / U_ref
+
+        A_1_int = inte.quad(integrand_0, 0.0, np.pi,args = (0.0))
+
+        A_1 = 2.0 / np.pi * A_1_int[0]
+
+    else:
+        Gamma_n = Gamma_N[n]
+        eta_n   = eta_N[n]
+        xi_n    = xi_N[n]
+        dndeta = pot.dndeta(Gamma_n, eta_n, xi_n, v_core, alpha_eff, c)
+
+        integrand_n = lambda theta: dndeta(theta) * np.cos(theta) / U_ref
+
+        A_1_int = inte.quad(integrand_n, 0.0, np.pi)
+
+        A_1 = A_1 + 2.0 / np.pi * A_1_int[0]
+
+# Calculate circulation of bound vortex
+Gamma_b = np.pi * c * U_ref * (A_0 + A_1 * 0.5)
+
+cl = np.pi * (2 * A_0+ A_1)
+
+print(A_0)
+print(A_1)
+print(Gamma_b)
+print(cl)
---
+++ aero_solver/test1.py
@@ -0,0 +1,9 @@
+import math
+import scipy
+import scipy.integrate
+import potential_flow as pot
+
+c = 1
+
+trans = pot.lambda_theta_2_xi(1)
+
