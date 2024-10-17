acac45dd8a6e7f4105320937d68087b880476f4b
ibetheeostrich <ibetheeostrich@gmail.com>
2024-10-18 07:40:24 +1100

    results more

--- main.py
+++ main.py
@@ -240,7 +240,7 @@ def main(alpha,U_ref,frequency,lesp):
 
 if __name__ == "__main__":
 
-    alpha = 2.5
+    alpha = 0.0
 
     Uref = [8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0]
     freq = [1.0, 1.5, 2.0, 2.5, 3.0]
